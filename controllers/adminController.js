8c8c6a7b50dda09adda7adfaf7268d0ad8a336a1
BlackCracker99 <fogxcfg@gmail.com>
2023-09-14 10:48:28 +0530

    admin sidebar icons add

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -13,29 +13,29 @@ const dashboard = asyncHandler(async (req, res) => {
         {
           text: "Filters",
           link: "./index.html",
-          icon: "ti ti-layout-dashboard",
+          icon: "ti ti-filter",
           subMenues: ["Grade","class","slider","notice"],
         },
         {
           text: "Students",
           link: "./ui-buttons.html",
-          icon: "ti ti-layout-dashboard",
+          icon: "ti ti-user",
         },
         {
           text: "Add Student Index",
           link: "./ui-buttons.html",
-          icon: "ti ti-layout-dashboard",
+          icon: "ti ti-plus",
         },
         {
           text: "Student Cards",
           link: "./ui-buttons.html",
-          icon: "ti ti-article",
+          icon: "ti ti-credit-card",
           subMenues: ["Online - Group 1","Kurunegala","Negambo - Group 1","Negambo - Group 2"],
         },
         {
           text: "Academy",
           link: "./ui-buttons.html",
-          icon: "ti ti-article",
+          icon: "ti ti-book",
           subMenues: ["Teachers","Class Schedule","Class Tute","Video Lessons"],
         },
         {
@@ -47,34 +47,34 @@ const dashboard = asyncHandler(async (req, res) => {
         {
           text: "Student Attendance",
           link: "./ui-buttons.html",
-          icon: "ti ti-article",
+          icon: "ti ti-files",
           subMenues: ["Online Class","Physical Class","Free Class","Attendance Reports"],
         },
         {
           text: "SMS",
           link: "./ui-buttons.html",
           subMenues: ["Not Paid Students","Paid Students","Pending Payment Students"],
-          icon: "ti ti-layout-dashboard",
+          icon: "ti ti-folder",
         },
         {
           text: "Reviews",
           link: "./ui-buttons.html",
-          icon: "ti ti-layout-dashboard",
+          icon: "ti ti-id-badge",
         },
         {
           text: "Gallary",
           link: "./ui-buttons.html",
-          icon: "ti ti-layout-dashboard",
+          icon: "ti ti-files",
         },
         {
           text: "Mail",
           link: "./ui-buttons.html",
-          icon: "ti ti-layout-dashboard",
+          icon: "ti ti-bookmark",
         },
         {
           text: "Admin",
           link: "./ui-buttons.html",
-          icon: "ti ti-layout-dashboard",
+          icon: "ti ti-shield",
         },
       ];
     
